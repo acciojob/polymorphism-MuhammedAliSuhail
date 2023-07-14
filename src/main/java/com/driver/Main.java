7c2d4269db04c88bb05a0802cb2d4f61764ab52f
MuhammedAliSuhail <alisuhail3454@gmail.com>
2023-07-14 18:59:34 +0100

    final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,10 @@
 package com.driver;
-import java.util.*;
+
 public class Main {
 
 
 
-    public  static class Product{
+    public static  class Product{
 
         public int product(int x, int y) {
 
@@ -24,15 +24,15 @@ public class Main {
 
   
     public static void main(String[] args) {
-             Product p=new Product();
-Scanner sc=new Scanner(System.in);
-int x=sc.nextInt();
-int y=sc.nextInt();
-int z=sc.nextInt();
-       int a= p.product(x,y);
-       int b=p.product(x,y,z);
-       double n=sc.nextDouble();
-       double m=sc.nextDouble();
-       double c=p.product(n,m);
+        Product p=new Product();
+
+
+      p.product(30,5);
+       p.product(39.32,1.0);
+      p.product(480,0,0);
     }
+    
+      
+    
+
 }
